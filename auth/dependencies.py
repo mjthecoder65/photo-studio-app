6769362661ff2fe140b4c8505d53a8f1e57ab524
mjthecoder65 <michael53161@gmail.com>
2025-12-16 20:47:17 +0900

    refactor: removed unused dependencies

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
